a73300ace6d9cbc6ca69301adb974767b7098546
Zhou Haochen <166598222+GDGZDYY@users.noreply.github.com>
2024-06-14 03:47:15 +0800

    怪制作+史莱姆跟随+怪出生点设置
    怪制作+史莱姆跟随+怪出生点设置+杂七杂八的，三个bug注释掉了要后解决

---
+++ Project/Assets/sprites/Guns/AnimatorHash.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class AnimatorHash 
+{
+    public static readonly int MoveSpeed = Animator.StringToHash(name: "Move Speed");
+    public static readonly int IsShooting = Animator.StringToHash(name: "Is Shooting");
+    public static readonly int IsMoving = Animator.StringToHash(name: "Is Movin");
+}
