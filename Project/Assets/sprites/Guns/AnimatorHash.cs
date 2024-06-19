efbc6b5e44dfb7835b2d534a76ea01ea1e09c6e5
Zhou Haochen <166598222+GDGZDYY@users.noreply.github.com>
2024-06-20 00:02:41 +0800

    更新
    怪更新+哥布林跟踪+跟主角有关的代码移到player

--- Project/Assets/sprites/Guns/AnimatorHash.cs
+++ Project/Assets/sprites/Guns/AnimatorHash.cs
@@ -4,5 +4,5 @@ public class AnimatorHash1
 {
     public static readonly int MoveSpeed = Animator.StringToHash(name: "Move Speed");
     public static readonly int IsShooting = Animator.StringToHash(name: "Is Shooting");
-    public static readonly int IsMoving = Animator.StringToHash(name: "Is Movin");
+    public static readonly int IsMoving = Animator.StringToHash(name: "Is Moving");
 }
