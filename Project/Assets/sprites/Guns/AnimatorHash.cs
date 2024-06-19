1984b20880e3bdc9a8da8bdbee2bd256988427cd
Zhou Haochen <166598222+GDGZDYY@users.noreply.github.com>
2024-06-20 01:59:45 +0800

    Delete Project directory
    有问题

--- Project/Assets/sprites/Guns/AnimatorHash.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class AnimatorHash
-{
-    public static readonly int MoveSpeed = Animator.StringToHash(name: "Move Speed");
-    public static readonly int IsShooting = Animator.StringToHash(name: "Is Shooting");
-    public static readonly int IsMoving = Animator.StringToHash(name: "Is Moving");
-}
--- Project/Assets/sprites/wave/wave.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-[Serializable]
-public class wave
-{
-    public Beetle beetle;
-    public int count;
-    public float timeBetweenSpawn;
-}
