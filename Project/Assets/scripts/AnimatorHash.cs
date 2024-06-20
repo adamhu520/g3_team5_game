d99d0287e7497a9ea7cc177039e91a61eddfaf7b
huzhihan <1592859558@qq.com>
2024-06-20 14:50:27 +0800

    回档
    da52c57d9fa732c7efd2f7a8efcbcec76ba1cc24

---
+++ Project/Assets/scripts/AnimatorHash.cs
@@ -0,0 +1,8 @@
+
+using UnityEngine;
+
+public static class AnimatorHash
+{
+    public static readonly int MoveSpeed = Animator.StringToHash("Move Speed");
+    public static readonly int IsShooting = Animator.StringToHash("IsShooting");
+}
